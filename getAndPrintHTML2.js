b69243eff00b89c13a35bc3b8c5039d70f096c6e
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-08 19:10:06 +0000

    initial commit

---
+++ getAndPrintHTML2.js
@@ -0,0 +1,27 @@
+var https = require('https'); 
+
+function getAndPrintHTML(options) {
+  var emptyString = "";
+
+
+  https.get(options, function (response) {
+  response.setEncoding('utf8');
+
+  response.on('data', function (data) {
+    console.log('Chunk Received. Length:', data + "\n");
+    emptyString += data;
+  });
+
+  response.on('end', function() {
+    console.log('Response stream complete.');
+  });
+
+});
+
+}
+
+getAndPrintHTML({
+  host: 'sytantris.github.io',
+  path: '/http-examples/step1.html'
+});
+

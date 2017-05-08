74a393a88f53b477dc97ddd2a1ba616166dcda21
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-08 20:14:22 +0000

    initial commit

---
+++ getHTML.js
@@ -0,0 +1,33 @@
+var https = require('https');
+
+function getHTML (options, callback) {
+var html = "";
+
+
+  https.get(options, function (response) {
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data + "\n");
+      html += data;
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+ 
+getHTML (requestOptions, printHTML);

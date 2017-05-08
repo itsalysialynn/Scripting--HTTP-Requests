3fac6c96e0a6de1188ca82240802bd12bf914e7a
Alysia Lynn <itsalysialynn-GitHub@gmail.com>
2017-05-08 19:38:12 +0000

    spacing changes

--- getAndPrintHTML2.js
+++ getAndPrintHTML2.js
@@ -1,22 +1,22 @@
-var https = require('https'); 
+var https = require('https');
 
 function getAndPrintHTML(options) {
   var emptyString = "";
 
 
   https.get(options, function (response) {
-  response.setEncoding('utf8');
+    response.setEncoding('utf8');
 
-  response.on('data', function (data) {
-    console.log('Chunk Received. Length:', data + "\n");
-    emptyString += data;
+    response.on('data', function (data) {
+      console.log('Chunk Received. Length:', data + "\n");
+      emptyString += data;
+    });
+
+    response.on('end', function() {
+      console.log('Response stream complete.');
+    });
+
   });
-
-  response.on('end', function() {
-    console.log('Response stream complete.');
-  });
-
-});
 
 }
 
